402d698f49cc1d20877ab4b27e438e77a7f17a5d
Alan Terriaga <alanst32@gmail.com>
2023-06-28 22:51:59 +1000

    chore: project created with projen

--- src/infra/common/constants/aws.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A class containing labeled references to every AWS Account in the organization
- */
-export declare class AccountsOrganization {
-  static readonly ORG_NON_PROD = "590312749310";
-  static readonly ORG_PROD = "590312749310";
-}

7433dc406fb633ef5c593ecd884e9efc48211b07
Alan Terriaga <alanst32@gmail.com>
2023-06-28 22:35:18 +1000

    feat(Config): Initial config

--- .projenrc.js
+++ .projenrc.js
@@ -7,5 +7,52 @@ const project = new typescript.TypeScriptProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  // repository: ''
 });
+
+// Git Ignore
+project.gitignore.addPatterns('cdk.context.json', '.idea', '*.iml', '.DS_Store', '.vscode/');
+
+// eslint
+project.eslint?.addPlugins('no-relative-import-paths');
+project.eslint?.addRules({
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      devDependencies: ['**/testing/**', '**/*.test.ts', 'projenrc/**', 'src/infra/**'],
+    },
+  ],
+});
+
+project.eslint?.addRules({
+  'import/order': [
+    'error',
+    {
+      'alphabetize': {
+        order: 'asc',
+        caseInsensitive: true,
+      },
+      'newlines-between': 'always',
+    },
+  ],
+});
+
+project.eslint?.addOverride({
+  files: ['*.test.ts', 'projenrc/**/*', 'src/infra/**/*', '.projenrc.ts'],
+  rules: {
+    'no-relative-import-paths/no-relative-import-paths': 'off',
+  },
+});
+
+// TS Config
+const tsConfigs = [project.tsconfig, project.tsconfigDev];
+tsConfigs.forEach((config) => {
+  config?.file.addOverride('ts-node.require', ['tsconfig-paths/register']);
+  config?.file.addOverride('compilerOptions.baseUrl', '.');
+  config?.file.addOverride('compilerOptions.lib', ['ES2020', 'dom']);
+  config?.file.addOverride('compilerOptions.target', 'ES2020');
+  config?.file.addOverride('compilerOptions.paths', cdkAppTsconfigPaths);
+});
+
 project.synth();
---
+++ src/infra/common/constants/aws.ts
@@ -0,0 +1,7 @@
+/**
+ * A class containing labeled references to every AWS Account in the organization
+ */
+export declare class AccountsOrganization {
+  static readonly ORG_NON_PROD = "590312749310";
+  static readonly ORG_PROD = "590312749310";
+}

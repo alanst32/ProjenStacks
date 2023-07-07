49df8947fcb402be6781e41bbfec00c03cd20bac
Alan Terriaga <alanst32@gmail.com>
2023-07-07 20:11:03 +1000

    feat(Infra) Msg

--- projenrc/coverage-badge/coverage-badges.ts
+++ projenrc/coverage-badge/coverage-badges.ts
@@ -1,22 +1,17 @@
-import { javascript } from "projen";
-import { AwsCdkTypeScriptApp } from "projen/lib/awscdk";
+import { javascript } from 'projen';
+import { AwsCdkTypeScriptApp } from 'projen/lib/awscdk';
 
-import {
-  CoverageBadgesBase,
-  CoverageBadgesOptions,
-} from "./coverage-badges-base";
+import { CoverageBadgesBase, CoverageBadgesOptions } from './coverage-badges-base';
 
 export class CoverageBadges extends CoverageBadgesBase {
   constructor(project: AwsCdkTypeScriptApp, options?: CoverageBadgesOptions) {
     const updateCoverageReporters = (reqReporters: string[]) => {
       const jestConfig: javascript.JestConfigOptions = project.jest?.config;
       const existingReporters = jestConfig?.coverageReporters || [];
-      const missingReporters = reqReporters.filter(
-        (reporter) => !existingReporters.find((r) => r === reporter)
-      );
+      const missingReporters = reqReporters.filter(reporter => !existingReporters.find(r => r === reporter));
       jestConfig?.coverageReporters?.push(...missingReporters); // assumes coverageReporters are initialised
     };
 
-    super(project, "jest test --coverage", updateCoverageReporters, options);
+    super(project, 'jest test --coverage', updateCoverageReporters, options);
   }
 }
